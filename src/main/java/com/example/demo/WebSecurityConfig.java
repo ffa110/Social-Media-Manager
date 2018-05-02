1c3d24327a1813d05684905429b1c594c2cea509
ffa110 <ffabunayyan@hotmail.com>
2018-05-02 00:34:42 -0400

    Login redirects users to the appropriate url after successful login

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -40,7 +40,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
-                .loginPage("/login")
+                .loginPage("/login").successForwardUrl("/media")
                 .permitAll()
                 .and()
             .logout().logoutSuccessHandler(customizeLogoutSuccessHandler)
